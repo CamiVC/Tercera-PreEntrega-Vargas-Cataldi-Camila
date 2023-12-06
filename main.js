38c6c5ce284b804030aa4d3e9e7071ecf45cc020
Camila Vargas Cataldi <camlor10var@gmail.com>
2023-12-05 21:00:47 -0300

    Agregué array de mercadería

--- main.js
+++ main.js
@@ -1,5 +1,80 @@
 let buttonGuardar = document.getElementById("buttonGuardar")
 
+const mercaderia = [
+    {
+        id:1,
+        nombre: "Carpa Iglú",
+        precio: 10000,
+        categoria: "carpa",
+    },
+    {
+        id:2,
+        nombre: "Carpa Canadiense",
+        precio: 30000,
+        categoria: "carpa",
+    },
+    {
+        id:3,
+        nombre: "Carpa Alta Montaña",
+        precio: 50000,
+        categoria: "carpa",
+    },
+    {
+        id:4,
+        nombre: "Bolsa de dormir chica",
+        precio: 5000,
+        categoria: "bolsa",
+    },
+    {
+        id:5,
+        nombre: "Bolsa de dormir grande",
+        precio: 7000,
+        categoria: "bolsa",
+    },
+    {
+        id:6,
+        nombre: "Bolsa de dormir Alta Montaña",
+        precio: 9000,
+        categoria: "bolsa",
+    },
+    {
+        id:7,
+        nombre: "Botas",
+        precio: 3000,
+        categoria: "calzado",
+    },
+    {
+        id:8,
+        nombre: "Zapatillas",
+        precio: 2000,
+        categoria: "calzado",
+    },
+    {
+        id:9,
+        nombre: "Sandalias",
+        precio: 1000,
+        categoria: "calzado",
+    },
+    {
+        id:10,
+        nombre: "Mochila campamento 45 Litros",
+        precio: 10000,
+        categoria: "mochila",
+    },
+    {
+        id:11,
+        nombre: "Mochila campamento 60 Litros",
+        precio: 11000,
+        categoria: "mochila",
+    },
+    {
+        id:12,
+        nombre: "Mochila campamento 80 Litros",
+        precio: 12000,
+        categoria: "mochila",
+    }
+]
+
 let carrito
 const carritoLS = localStorage.getItem("Carrito")
 
