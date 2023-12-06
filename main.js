aa22f5e613b570ca63e1a12dc256f1cd72b8e8e4
Camila Vargas Cataldi <camlor10var@gmail.com>
2023-12-05 21:53:20 -0300

    Agregué imágenes y creé cards con render

--- main.js
+++ main.js
@@ -4,72 +4,84 @@ const mercaderia = [
     {
         id:1,
         nombre: "Carpa Iglú",
+        img:"./images/carpa iglu.jpg",
         precio: 10000,
         categoria: "carpa",
     },
     {
         id:2,
         nombre: "Carpa Canadiense",
+        img:"./images/carpa canadiense.jpg",
         precio: 30000,
         categoria: "carpa",
     },
     {
         id:3,
         nombre: "Carpa Alta Montaña",
+        img:"./images/carpa alta montaña.jpg",
         precio: 50000,
         categoria: "carpa",
     },
     {
         id:4,
         nombre: "Bolsa de dormir chica",
+        img:"./images/bolsa chica.png",
         precio: 5000,
         categoria: "bolsa",
     },
     {
         id:5,
         nombre: "Bolsa de dormir grande",
+        img:"./images/bolsa grande.png",
         precio: 7000,
         categoria: "bolsa",
     },
     {
         id:6,
-        nombre: "Bolsa de dormir Alta Montaña",
+        nombre: "Bolsa de dormir Térmica",
+        img:"./images/bolsa termica.png",
         precio: 9000,
         categoria: "bolsa",
     },
     {
         id:7,
         nombre: "Botas",
+        img:"./images/botas.jpg",
         precio: 3000,
         categoria: "calzado",
     },
     {
         id:8,
         nombre: "Zapatillas",
+        img:"./images/zapatillas.jpg",
         precio: 2000,
         categoria: "calzado",
     },
     {
         id:9,
         nombre: "Sandalias",
+        img:"./images/sandalias.jpg",
         precio: 1000,
         categoria: "calzado",
     },
     {
         id:10,
         nombre: "Mochila campamento 45 Litros",
+        img:"./images/mochila 45.jpg",
         precio: 10000,
         categoria: "mochila",
     },
     {
         id:11,
         nombre: "Mochila campamento 60 Litros",
+        img:"./images/mochila 60.jpg",
         precio: 11000,
         categoria: "mochila",
     },
     {
         id:12,
         nombre: "Mochila campamento 80 Litros",
+        img:"./images/Mochila 80.jpg",
         precio: 12000,
         categoria: "mochila",
     }
@@ -78,23 +90,24 @@ const mercaderia = [
 let carrito
 const carritoLS = localStorage.getItem("Carrito")
 
-class producto {
-    constructor (nombre, precio, cantidad){
-        this.nombre = nombre
-        this.precio = precio
-        this. cantidad = cantidad
-    }
-}
-
 if(carritoLS){
     carrito = JSON.parse(carritoLS)
 } else { 
     carrito = []
 }
 
-buttonGuardar.onclick = () => {
-    const compra = new producto (document.getElementById("inputProducto").value, document.getElementById("inputPrecio").value, document.getElementById("inputCantidad").value)
-    carrito.push(compra)
-    console.log(carrito)
-    localStorage.setItem("Carrito", JSON.stringify(carrito))
+let productoContainer = document.getElementById("productoContainer")
+
+function renderCarrito (mercaderia){
+    mercaderia.forEach(producto => {
+        const card = document.createElement("div")
+        card.innerHTML =   `<h3>${producto.nombre}</h3>
+                        <img src="${producto.img}" alt="${producto.nombre}">
+                        <p>Precio: $${producto.precio}`
+        productoContainer.appendChild(card)
+    })
 }
+
+renderCarrito(mercaderia)
+
+
